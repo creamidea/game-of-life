9906418635a9d568243c89cb772bcbe934965f36
NekoTrek <creamidea@gmail.com>
2020-10-05 20:18:52 +0800

    feat: init the project

---
+++ index.js
@@ -0,0 +1,78 @@
+import { Universe } from "wasm-game-of-life";
+import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg.wasm';
+
+const CELL_SIZE = 5; // px
+const DEAD = 0;
+const GRID_COLOR = '#CCCCCC';
+const DEAD_COLOR = '#FFFFFF';
+const ALIVE_COLOR = '#000000';
+
+const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
+
+const canvas = document.querySelector('#game-of-life-canvas');
+canvas.width = (CELL_SIZE + 1) * width + 1;
+canvas.height = (CELL_SIZE + 1) * height + 1;
+
+const ctx = canvas.getContext('2d');
+
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.lineWidth = 1 / window.devicePixelRatio;
+  ctx.strokeStyle = GRID_COLOR;
+
+  // 水平线
+  for(let i = 0; i < width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  for(let i = 0; i < height; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, i * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
+}
+
+const getIndex = (row, col) => {
+  return row * width + col;
+}
+
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      ctx.fillStyle = cells[idx] === DEAD ? DEAD_COLOR : ALIVE_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE,
+      )
+    }
+  }
+
+  ctx.stroke();
+}
+
+const renderLoop = () => {
+  universe.tick();
+
+  drawGrid();
+  drawCells();
+
+  requestAnimationFrame(renderLoop);
+}
+
+drawGrid();
+drawCells();
+requestAnimationFrame(renderLoop);
